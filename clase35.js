b9cfe823c264f9accb47808c25b3f38b7a7cbd1a
Malena Rey <36973939+malerey@users.noreply.github.com>
2019-11-04 10:42:12 -0300

    Update clase35.js

--- clase35.js
+++ clase35.js
@@ -1,3 +1,12 @@
+// Flo, este codigo tiene el mismo problema que el ej 34. Supongo que con Number lo que intentás hacer es validar
+// si efectivamente cada variable es un número. Eso no funciona, ya que javascript no sabe bien qué hacer con
+// Number (supone que es una variable) y lo ignora. Tu código funcionaría sacando todas las menciones a Number. 
+
+// Para validar si nota1 es un número, deberíamos hacer algo así
+
+// const nota1 = prompt("Por favor, ingrese la nota correspondiente al TP 1")
+// Number(nota1) || alert("Por favor ingrese un número válido") 
+
 const nota1 = Number && prompt("Por favor, ingrese la nota correspondiente al TP 1") 
 || (alert(`Por favor, ingrese un numero entre el 0 y el 100`),
 prompt(`Por favor, ingrese la nota correspondiente al TP 1`));
@@ -13,6 +22,11 @@ const nota3 = Number && prompt("Por favor, ingrese la nota correspondiente al TP
 prompt(`Por favor, ingrese la nota correspondiente al TP 3`));
 console.log(nota3);
 
+// Multiplicar los números por 1 supongo que es una manera de coercionar la variable nota1, que es un prompt, 
+// a que sea un número. 
+// No está mal en sí, ya que "funciona", pero no es el método más claro para otra persona que lee nuestro código. 
+// Existe el método Number() cuya función es justamente hacer esa coerción, por lo que sería preferible hacerlo así
+// let sumaNotas = Number(nota1) + Number(nota2) + Number(nota3)
 let sumaNotas = Number = nota1*1 + nota2*1 + nota3*1;
 console.log(sumaNotas);
 
@@ -27,3 +41,5 @@ let notaFinal = ((promedio > 90) && "EXCELENTE") ||
 console.log(notaFinal)
 
 alert(`El promedio de notas es ${notaFinal}.`);
+
+// Muy buen trabajo
